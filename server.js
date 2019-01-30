652341225a70d953d07b32e834870c707623a18d
466179464@qq.com <466179464@qq.com>
2019-01-30 10:11:06 +0800

    set environment

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 "use strict";
+//Detect environment
+const path = require('path')
+const compression = require('compression')
+const isDeveloping = process.env.NODE_ENV !== 'production'
+console.log('--------222',process.env.NODE_ENV)
+const port = isDeveloping ? 3000 : process.env.PORT
 
 // Generate webpack config with CLI service
 const webpackConfig = require("@vue/cli-service/webpack.config.js");
@@ -6,26 +12,36 @@ const webpackConfig = require("@vue/cli-service/webpack.config.js");
 // Create express app
 const express = require("express");
 const app = express();
+if(isDeveloping){
+    // Configure webpack as middleware
+    const webpack = require("webpack");
 
-// Configure webpack as middleware
-const webpack = require("webpack");
+    webpackConfig.entry.app.unshift('webpack-hot-middleware/client');
+    const compiler = webpack(webpackConfig);
+    const devMiddleware = require('webpack-dev-middleware'); // eslint-disable-line
+    app.use(devMiddleware(compiler, {
+        noInfo: false,
+        publicPath: webpackConfig.output.publicPath,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        stats: {colors: true}
+    }));
 
-webpackConfig.entry.app.unshift('webpack-hot-middleware/client');
-const compiler = webpack(webpackConfig);
-const devMiddleware = require('webpack-dev-middleware'); // eslint-disable-line
-app.use(devMiddleware(compiler, {
-	noInfo: false,
-	publicPath: webpackConfig.output.publicPath,
-	headers: { "Access-Control-Allow-Origin": "*" },
-	stats: {colors: true}
-}));
+    const hotMiddleware = require('webpack-hot-middleware'); // eslint-disable-line
+    app.use(hotMiddleware(compiler, {
+        log: console.log
+    }));
+}else{
+    app.use(compression());
+    app.use(express.static(__dirname + '/dist'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist/index.html'))
+    })
+}
 
-const hotMiddleware = require('webpack-hot-middleware'); // eslint-disable-line
-app.use(hotMiddleware(compiler, {
-	log: console.log
-}));
 
-const port = 8080;
-app.listen(port, function() {
-	console.log("Developer server running on http://localhost:" + port);
-});
+app.listen(port, '0.0.0.0', function onStart(err) {
+    if(err){
+        console.log(err)
+    }
+    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+})
